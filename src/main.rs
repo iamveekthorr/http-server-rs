03c5d6d271034bd1dfec0c3fbe27cabd82ce9493
iamveekthorr <victorokonkwo67@gmail.com>
2025-07-17 18:14:41 -0500

    chore:  setup connection handler

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{BufReader, prelude::*},
     net::{TcpListener, TcpStream},
 };
@@ -14,11 +15,21 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    println!("Request: {:#?}", http_request);
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK\r\n\r\n";
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let length = contents.len();
+
+        let response = format!(
+            "{status_line}\r\n
+        content-length: {length}\r\n\r\n\
+        {contents}"
+        );
+
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        // TODO: add body of the code
+    }
 }
