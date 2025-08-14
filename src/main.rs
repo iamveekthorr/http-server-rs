e9c6420dd095ab444e2ce0dea53a8014f51af35e
iamveekthorr <victorokonkwo67@gmail.com>
2025-08-14 20:59:15 +0100

    chore: complete the build

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use web_server_chappter_20::ThreadPool;
 fn main() {
     let listener = match TcpListener::bind("127.0.0.1:7878") {
         Ok(l) => l,
-        Err(_) => panic!("Error creating listener"),
+        Err(_) => return,
     };
 
     let pool = ThreadPool::new(4);
@@ -18,19 +18,20 @@ fn main() {
             Ok(stream) => pool.execute(|| {
                 handle_connection(stream);
             }),
-            Err(err) => panic!("error: {}", err),
+            Err(_) => return,
         };
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let request_line = String::new();
 
-    if let Some(request_line) = buf_reader.lines().next() {
-        match request_line {
+    let request_line: String;
+
+    if let Some(line) = buf_reader.lines().next() {
+        request_line = match line {
             Ok(line) => line,
-            Err(_) => panic!("Could not read reuqest line"),
+            Err(_) => return,
         }
     } else {
         panic!("Unable to read request line")
@@ -41,14 +42,21 @@ fn handle_connection(mut stream: TcpStream) {
         _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
+    let contents = match fs::read_to_string(filename) {
+        Ok(content) => content,
+        Err(_) => return,
+    };
+
     let length = contents.len();
 
     let response = format!(
         "{status_line}\r\n\
-            Content-Length: {length}\r\n\r\n
-            {contents}"
+        Content-Length: {length}\r\n\r\n
+        {contents}"
     );
 
-    stream.write_all(response.as_bytes()).unwrap();
+    match stream.write_all(response.as_bytes()) {
+        Ok(res) => res,
+        Err(_) => panic!("Couldn't write to stream"),
+    };
 }
